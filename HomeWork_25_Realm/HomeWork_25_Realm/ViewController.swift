b9068ed2be95ac9b79b629ae686a128202dde023
UladzimirKulakou <rombik9@ya.ru>
2021-08-08 00:15:30 +0300

    create Project

---
+++ HomeWork_25_Realm/HomeWork_25_Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork_25_Realm
+//
+//  Created by Владимир  on 8/8/21.
+//  Copyright © 2021 Владимир . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
