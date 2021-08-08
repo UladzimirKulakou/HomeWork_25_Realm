2f99b9cd11867696e94c3f98b2bc79a477a81990
UladzimirKulakou <rombik9@ya.ru>
2021-08-08 23:24:52 +0300

    it is work
    table dosn't update without sort

---
+++ HomeWork_25_Realm/HomeWork_25_Realm/Extensions/UITableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell.swift
+//  HomeWork_25_Realm
+//
+//  Created by Владимир  on 8.08.21.
+//  Copyright © 2021 Владимир . All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+}
